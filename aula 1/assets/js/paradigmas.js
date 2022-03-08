8ddde97a9f711c69f6ad03b7c938e2ab5c0c2aff
Gabriel-Leao <gabriellsilva1200@outlook.com.br>
2022-03-08 16:03:12 -0300

    chegou a luz

---
+++ aula 1/assets/js/paradigmas.js
@@ -0,0 +1,9 @@
+// Sintaxe literal (Básica)
+// Implícito
+const escola = "Let's Code"
+console.log(escola, typeof(escola), escola.constructor)
+
+// Sintaxe OOP (Avançada)
+// Explícito
+const school = new String ("Let's Code")
+console.log(school, typeof(school), school.constructor)
