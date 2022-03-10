1a0f0b5b4198909db036996463b71bed5e2836ad
Gabriel-Leao <gabriellsilva1200@outlook.com.br>
2022-03-10 16:37:10 -0300

    update

--- aula 1/assets/js/paradigmas.js
+++ aula 1/assets/js/paradigmas.js
@@ -3,7 +3,32 @@
 const escola = "Let's Code"
 console.log(escola, typeof(escola), escola.constructor)
 
+const desde = 2015; // number literal
+console.log(desde, typeof desde, desde.constructor);
+
 // Sintaxe OOP (Avançada)
 // Explícito
 const school = new String ("Let's Code")
 console.log(school, typeof(school), school.constructor)
+
+const since = new Number(2015);
+console.log(since, typeof since, since.constructor);
+
+
+// API Javascript
+console.dir(String.prototype);
+console.dir(Number.prototype);
+console.dir(Array.prototype);
+console.dir(Boolean.prototype);
+console.dir(Function.prototype);
+console.dir(Object.prototype);
+console.dir(Date.prototype);
+console.dir(RegExp.prototype);
+console.dir(Math);
+
+// Exemplos
+console.log(escola.toUpperCase());
+console.log(school.toUpperCase());
+
+console.log(escola.length);
+console.log(escola.padEnd(10, " ").repeat(5).toUpperCase());
