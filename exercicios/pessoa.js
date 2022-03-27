619200c93d92b5598162e27d0859b296ffa5ab66
Gabriel-Leao <gabriellsilva1200@outlook.com.br>
2022-03-27 17:16:05 -0300

    update

--- exercicios/pessoa.js
+++ exercicios/pessoa.js
@@ -7,23 +7,24 @@ const humano = {
     idade: 23,
     peso: 57,
     altura: 1.66,
-    comer: function() {
-        let cal = input('Digite as calorias consumidas: ')
+    comer: function(cal) {
         this.movimentos.push(`Consomiu ${cal} calorias`)
         if (cal >= 7000) {
             return ++this.peso
         } else return this.peso
     },
-    treinar: function() {
-        let gasto = input('Digite as calorias gastadas: ')
+    treinar: function(gasto) {
+        this.movimentos.push(`Gastou ${gasto} calorias`)
         if (gasto >= 7000) {
             return --this.peso
         } else return this.peso
     },
-    movimentos: []
+    movimentos: new Array
 }
 
-humano.comer()
-humano.treinar()
+let cal = input('Digite as calorias consumidas: ')
+humano.comer(cal)
+let gasto = input('Digite as calorias gastadas: ')
+humano.treinar(gasto)
 console.log(`O peso de ${humano.nome} é de ${humano.peso}KG`)
 console.log(humano.movimentos)
