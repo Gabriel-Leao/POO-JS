91453d0de1d8ae883b3b0611356602b656eba242
Gabriel-Leao <gabriellsilva1200@outlook.com.br>
2022-03-27 17:08:41 -0300

    update

--- exercicios/tv.js
+++ exercicios/tv.js
@@ -1,48 +1,68 @@
-/* Construa uma classe para gerar um objeto TV com as propriedades canal, volume e ligada e métodos liga, desliga, 
-udaDeCanal e aumentaVolume, diminuiVolume.
-Uma implementação publica e outra privada. */
-
 class TV {
-    constructor() {
-        this.volume = {
+    constructor(marca="", canal=5, volume=10, polegadas=40, estado=false) {
+        this.#marca = marca
+        this.#canal = canal
+        this.#volume = {
             min: 0,
             max: 100,
-            atual: 10
+            atual: volume
         }
-        this.canal = 5
-        this.ligada = false
+        this.#polegadas = polegadas
+        this.#estado = estado
+    }
+
+    #volume
+    #canal
+    #estado
+    #polegadas
+    #marca
+
+    static canais = {
+        2: "Cultura",
+        4: "SBT",
+        5: "Globo",
+        7: "Record",
+        9: "RedeTv",
+        11: "Gazeta",
+        13: "Band"
     }
 
     ligarDesligar() {
-        return this.ligada = !this.ligada
+        return this.#estado = !this.#estado
     }
 
-    mudarCanal(numeroCanal) {
-        if (numeroCanal < 2 || numeroCanal > 64) {
-            console.log("Canal não existe, os números dos canais estão entre 2 e 64")
-            return this.canal
-        } else return this.canal = numeroCanal
+    mudarCanal(numeroCanal=this.#canal) {
+        if(this.#estado &&  TV.canais[numeroCanal] == undefined) {
+            return "Canal não encontrado"
+        } else if (this.#estado) {
+            this.#canal = numeroCanal
+            return TV.canais[this.#canal]
+        } else return `Sua TV ${this.#marca} de ${this.#polegadas} polegadas está desligada`
     }
 
     aumentarVolume () {
-        if (this.volume.atual == this.volume.max) {
-            return this.volume.atual
-        } else if (this.ligada) {
-            return  this.volume.atual += 1
+        if (this.#volume.atual == this.#volume.max) {
+            return this.#volume.atual
+        } else if (this.#estado) {
+            return  this.#volume.atual += 1
         } else {
-            return this.volume.atual = 0
+            return `Sua TV ${this.#marca} de ${this.#polegadas} polegadas está desligada`
         }
     }
 
     diminuirVolume() {
-        if (this.volume.atual == this.volume.min) {
-            return  this.volume.atual
-        } else if (this.ligada) {
-            return  this.volume.atual -= 1
+        if (this.#volume.atual == this.#volume.min) {
+            return  this.#volume.atual
+        } else if (this.#estado) {
+            return  this.#volume.atual -= 1
         } else {
-            return this.volume.atual = 0
+            return `Sua TV ${this.#marca} de ${this.#polegadas} polegadas está desligada`
         }
+    }
+
+    info() {
+        return `A sua tv ${this.#marca} de ${this.#polegadas} polegadas está ${(this.#estado) ? `ligada e sintonizada no canal ${TV.canais[this.#canal]}`: `desligada`}`
     }
 }
 
-const casa = new TV
+const Samsung = new TV("Samsung", 4, 11, 50, true)
